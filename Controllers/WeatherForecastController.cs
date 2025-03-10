ea6168100f2c64b05b453c579bc1b4b815da9827
Terence Fan <stdrickforce@gmail.com>
2025-03-10 17:26:10 +0800

    update

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrossTenantSample.Controllers;
+
+[ApiController]
+[Route("/weather")]
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })];
+    }
+}
